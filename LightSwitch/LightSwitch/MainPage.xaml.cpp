4b3f7a7aff10de88de1d18f019ef63685040090a
ethanjsl <ethan.j.s.lyons@gmail.com>
2017-01-07 15:44:37 +0100

    initial commit

---
+++ LightSwitch/LightSwitch/MainPage.xaml.cpp
@@ -0,0 +1,58 @@
+﻿//
+// MainPage.xaml.cpp
+// Implementation of the MainPage class.
+//
+
+#include "pch.h"
+#include "MainPage.xaml.h"
+
+using namespace LightSwitch;
+
+using namespace Platform;
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::UI::Xaml::Controls::Primitives;
+using namespace Windows::UI::Xaml::Data;
+using namespace Windows::UI::Xaml::Input;
+using namespace Windows::UI::Xaml::Media;
+using namespace Windows::UI::Xaml::Navigation;
+using namespace Windows::Devices::Enumeration;
+using namespace Windows::Devices::Gpio;
+using namespace concurrency;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+MainPage::MainPage()
+{
+	InitializeComponent();
+
+	initGPIO(); //Initializes the GPIO
+}
+
+void MainPage::initGPIO() {
+	auto gpio = GpioController::GetDefault(); //Selects GPIOController
+
+	LED_pin_ = gpio->OpenPin(LED_PIN); //Opens a connection to the pin to control the LED
+	LED_pin_->Write(pinInitValue_);	//Set the initial value to the pin (GpioPinValue::Low)
+	LED_pin_->SetDriveMode(GpioPinDriveMode::Output); //Set pin as an output
+}
+
+// Name:		switchButton_Click()
+// Type:		Void LightSwitch::MainPage
+// Parameters:	Platform::Object sender (unused), Windows::UI::Xaml::RoutedEventArgs e (unused)
+// usage:		Is called when the light switch button (switchButton) is pressed.
+//				if the LED (LED_pin_) is off (GpioPinValue::Low), the LED is turned on (GpioPinValue::High)
+//				and the text on the light switch button is changed.
+void LightSwitch::MainPage::switchButton_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
+{
+	if (LED_pin_->Read() == GpioPinValue::Low) { //read the current value of the pin
+		LED_pin_->Write(GpioPinValue::High); //turn the pin on
+		switchButton->Content = "Turn Light Off"; //change the text on the light switch button
+	}
+	else {
+		LED_pin_->Write(GpioPinValue::Low); //turn the pin off
+		switchButton->Content = "Turn Light on"; //change the text on the light switch button
+	}
+}
---
+++ LightSwitch/LightSwitch/MainPage.xaml.h
@@ -0,0 +1,27 @@
+﻿//
+// MainPage.xaml.h
+// Declaration of the MainPage class.
+//
+
+#pragma once
+
+#include "MainPage.g.h"
+
+namespace LightSwitch
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public ref class MainPage sealed
+	{
+	public:
+		MainPage();
+
+	private:
+		void initGPIO(); //GPIO initialization method
+		Windows::Devices::Gpio::GpioPinValue pinInitValue_ = Windows::Devices::Gpio::GpioPinValue::Low; //Set the initial value for the LED
+		const int LED_PIN = 5; //Use GPIO5 as the LED pin
+		Windows::Devices::Gpio::GpioPin ^LED_pin_; //name the LED pin
+		void switchButton_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e); //Method used to detect if the light switch button is pressed
+	};
+}
